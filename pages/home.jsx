5798ead7dda7fc9ba88b8bf1b62f7a71ec9678af
sekharjavvadi <sekharatece@gmail.com>
2022-06-19 00:39:54 +0530

    home page ui completed with reponsive

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [count, setcount] = useState(0);
+  return (
+    <div className="home-page-parent">
+      <div className="nav-bar">
+        <div className="nav-bar-left">
+          <h1 className="heading">Holy Shit</h1>
+        </div>
+        <div className="nav-bar-right">
+          <img src="/opensea.png" alt="openSea" className="openSea pointer" />
+          <img
+            src="/etherscan.png"
+            alt="etherScan"
+            className="etherScan pointer"
+          />
+        </div>
+      </div>
+      <div className="home-body center-div">
+        <img src="/shitimages.png" alt="holy" className="holy-image pointer" />
+        <p className="is-live">Shitting is live</p>
+        <img
+          src="/shitbutton.png"
+          alt="shit-button"
+          className="shit-button pointer"
+        />
+        <p className="count">{count + " /4969"}</p>
+      </div>
+    </div>
+  );
 }
