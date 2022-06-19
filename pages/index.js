ea19b41b7ce4d5890934f93cd5b50d9bec1aa111
sekharjavvadi <sekharatece@gmail.com>
2022-06-19 21:49:17 +0530

    google analytics implemeted

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import HomePage from "./home";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-230631230-2"; // OUR_TRACKING_ID
 
+ReactGA.initialize(TRACKING_ID);
 export default function Home() {
   return <HomePage />;
 }
